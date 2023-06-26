42f5c103f73c5d2fd8360519d22370cda0c75796
DouniaBK <douniazedbacha@gmail.com>
2023-06-26 09:31:22 +0000

    Add aullauth accounts

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render
-from django.views import generic
+from django.shortcuts import render, get_object_or_404
+from django.views import generic, View
 from .models import Post
 
 # Create your views here.
@@ -11,3 +11,25 @@ class PostList(generic.ListView): # code for posts list in the blog
     template_name = "index.html"
     paginate_by = 6  # the number of posts on the page is set to 6
     # django will automatically create a second page for post number 7 and on
+
+
+
+class PostDetail(View):
+
+    def get(self, request, slug, *args, **kwargs):
+        queryset = Post.objects.filter(status=1)
+        post = get_object_or_404(queryset, slug=slug)
+        comments = post.comments.filter(approved=True).order_by("-created_on")
+        liked = False
+        if post.likes.filter(id=self.request.user.id).exists():
+            liked = True
+
+        return render(
+            request,
+            "post_detail.html",
+            {
+                "post": post,
+                "comments": comments,
+                "liked": liked
+            },
+        )
